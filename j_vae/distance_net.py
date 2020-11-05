38ee8273c0748c9d18bf0e8bf3c98fe78e8c28a9
erick <ga74kob@mytum.de>
2020-11-05 01:57:00 +0100

    more slots

---
+++ j_vae/distance_net.py
@@ -0,0 +1,17 @@
+def gen_training_data(N):
+    def get_data_points_pairs():
+        pass
+
+    #up to 10 bboxes;
+    def generate_bounding_boxes():
+        pass
+
+    # generate region
+
+    #generate points to measure distances (can surpass, (-1, 1) so it learns such cases are far) actually use region also to esitmate distance
+
+    #generate graph and process graph
+
+    #calculate distances between the points and save it a file
+
+    pass
