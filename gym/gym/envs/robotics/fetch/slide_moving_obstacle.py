87f33e018c2787a61274c17d14d77346ac3ef741
erick <ga74kob@mytum.de>
2021-01-14 12:36:51 +0100

    corrections slide env

--- gym/gym/envs/robotics/fetch/slide_moving_obstacle.py
+++ gym/gym/envs/robotics/fetch/slide_moving_obstacle.py
@@ -21,22 +21,22 @@ class FetchSlideMovingObstacleEnv(fetch_env.FetchEnv, utils.EzPickle):
         self.adapt_dict = dict()
         self.adapt_dict["field"] = [1.3, 0.75, 0.6, 0.25, 0.25, 0.2]
         # centers of the interval where goal and initial position will be sampled
-        self.target_goal_center = np.array([[1.7, 1.1, 0.42], [1.7, 0.2, 0.42]])
+        self.target_goal_center = np.array([[1.7, 1.12, 0.42], [1.7, 0.38, 0.42]])
         self.object_center = np.array([1.11, 0.75, 0.422])
         # for moving
         self.vel_lims = [0.8, 1.1]
         self.current_obstacle_vel = 2.1
         self.obstacle_direction = 1
         # the object must be in the middle from both limits in the xml
-        self.obstacle_upper_limit = 0.95
-        self.obstacle_lower_limit = 0.55
+        self.obstacle_upper_limit = 1.
+        self.obstacle_lower_limit = 0.5
         self.pos_dif = (self.obstacle_upper_limit - self.obstacle_lower_limit) / 2.
         #
         fetch_env.FetchEnv.__init__(
             self, MODEL_XML_PATH, has_object=True, block_gripper=True, n_substeps=20,
             gripper_extra_height=-0.02, target_in_the_air=False, target_offset=0.0,
             obj_range=np.array([0.02, 0.08]),
-            target_range=np.array([0.1, 0.05]), distance_threshold=0.05,
+            target_range=np.array([0.08, 0.05]), distance_threshold=0.05,
             initial_qpos=initial_qpos, reward_type=reward_type)
         utils.EzPickle.__init__(self)
         self.obstacle1_slider_idx = self.sim.model.joint_names.index('obstacle:joint')
@@ -135,7 +135,7 @@ class FetchSlideMovingObstacleEnv(fetch_env.FetchEnv, utils.EzPickle):
         obs = super(FetchSlideMovingObstacleEnv, self)._get_obs()
         body_id = self.sim.model.body_name2id('obstacle')
         pos1 = np.array(self.sim.data.body_xpos[body_id].copy())
-        dims = np.array([0.04, 0.22, 0.1])
+        dims = np.array([0.04, 0.2, 0.1])
         ob1 = np.concatenate((pos1, dims.copy()))
         obs['real_obstacle_info'] = np.array([ob1])
         obs['real_size_goal'] = np.array([0.05, 0.05, 0.02])
