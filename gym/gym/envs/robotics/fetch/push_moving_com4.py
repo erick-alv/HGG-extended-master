ba7651708786dd5c0973c82209a9ebe6b0a9194b
erick <ga74kob@mytum.de>
2021-01-25 23:07:25 +0100

    new vels 2

--- gym/gym/envs/robotics/fetch/push_moving_com4.py
+++ gym/gym/envs/robotics/fetch/push_moving_com4.py
@@ -19,8 +19,8 @@ class FetchPushMovingComEnv4(fetch_env.FetchEnv, utils.EzPickle):
         self.object_center = np.array([1.1, 0.65, 0.425])
 
         #for moving
-        self.vel_lims = [0.3, 0.5]
-        self.current_obstacle_vel = 0.3
+        self.vel_lims = [0.48, 0.625]
+        self.current_obstacle_vel = 0.48
         self.obstacle_direction = 1
         #the object must be in the middle from both limits in the xml
         self.obstacle_upper_limit = 1.46
